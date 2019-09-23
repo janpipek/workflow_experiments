f15da866bc63209860f33b6c051eb575673a977e
Jan Pipek <jan.pipek@gmail.com>
2019-09-23 20:55:50 +0200

    Luigi example1

--- luigi/example1.py
+++ luigi/example1.py
@@ -1,26 +1,55 @@
-"""Luigi example that mimicks `../dask_distributed/example1.py`"""
+"""Luigi example that mimicks `../dask_distributed/example1.py`
+
+You can run this example:
+- as a standalone script:
+
+    python example1.py 0 10
+
+- as a luigi task started by luigi itself:
+
+    luigi --module example1 SumNegSquareTask --local-scheduler --start 0 --stop 10
+
+Note: Unfortunately, the intermediate results are written to local files.
+"""
 
 import logging
+import os
+import sys
 
 import luigi
+
+basedir = os.path.join(os.path.dirname(__file__), "example1")
 
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 class SquareTask(luigi.Task):
-    x = luigi.FloatParameter()
+    x = luigi.IntParameter()
 
-    # TODO: Implement output
+    def output(self):
+        return luigi.LocalTarget(os.path.join(basedir, f"{self.x}-square.txt"))
+
+    def run(self):
+        with self.output().open("w") as outfile:
+            outfile.write(str(self.x ** 2))
 
 
 class NegSquareTask(luigi.Task):
-    x = luigi.FloatParameter()
+    x = luigi.IntParameter()
 
     def requires(self):
         return SquareTask(x=self.x)
 
-    # TODO: Implement output
+    def run(self):
+        with self.input().open("r") as infile:
+            squared = int(infile.read().strip())
+
+        with self.output().open("w") as outfile:
+            outfile.write(str(-squared))
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(basedir, f"{self.x}-neg_square.txt"))
 
 
 class SumNegSquareTask(luigi.Task):
@@ -30,4 +59,25 @@ class SumNegSquareTask(luigi.Task):
     def requires(self):
         yield from (NegSquareTask(x) for x in range(self.start, self.stop))
 
-    # TODO: Implement output
+    def run(self):
+        result = 0
+        for input in self.input():
+            with input.open("r") as infile:
+                result += int(infile.read().strip())
+
+        with self.output().open("w") as outfile:
+            outfile.write(str(result))
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(basedir, f"sum-{self.start}_to_{self.stop}.txt"))
+
+
+if __name__ == "__main__":
+    start, stop = (int(arg) for arg in sys.argv[1:3])
+    task = SumNegSquareTask(start=start, stop=stop)
+    result = luigi.build([task], local_scheduler=True)
+    
+    with task.output().open() as infile:
+        result = int(infile.read().strip())
+
+    print(f"Result: {result}")
